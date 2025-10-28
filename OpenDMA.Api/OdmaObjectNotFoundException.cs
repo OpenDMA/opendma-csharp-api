70809d255ead50eb7272c04cf62ec8f8a599bd3b
Stefan Kopf <s.kopf@xaldon.de>
2025-10-28 10:48:00 +0100

    Rework ObjectNotFoundException to distinguish between invalid repositoryId and objectId

--- OpenDMA.Api/OdmaObjectNotFoundException.cs
+++ OpenDMA.Api/OdmaObjectNotFoundException.cs
@@ -11,27 +11,43 @@ namespace OpenDMA.Api
     public class OdmaObjectNotFoundException : OdmaException
     {
 
-        private readonly OdmaGuid? _objectGuid;
+        private readonly OdmaId? _repositoryId;
+
+        private readonly OdmaId? _objectId;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="OdmaObjectNotFoundException"/> class.
         /// </summary>
-        /// <param name="objectGuid">The unique identifier of the object that was not found.</param>
-        public OdmaObjectNotFoundException(OdmaGuid objectGuid) 
-            : base(objectGuid != null ? "Object not found: "+objectGuid.ToString() : "Object not found")
+        /// <param name="repositoryId">The unique identifier of the repository that was not found.</param>
+        public OdmaObjectNotFoundException(OdmaId repositoryId) 
+            : base("Repository not found: `"+repositoryId+"`")
         {
-            this._objectGuid = objectGuid;
+            this._repositoryId = repositoryId;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OdmaObjectNotFoundException"/> class.
+        /// </summary>
+        /// <param name="repositoryId">The unique identifier of the existing repository.</param>
+        /// <param name="objectId">The unique identifier of the object that was not found.</param>
+        public OdmaObjectNotFoundException(OdmaId repositoryId, OdmaId objectId)
+            : base("Object `"+objectId+"` not found in repository `"+repositoryId+"`")
+        {
+            this._repositoryId = repositoryId;
+            this._objectId = objectId;
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="OdmaObjectNotFoundException"/> class with a specified error message.
         /// </summary>
         /// <param name="message">The message that describes the error.</param>
-        /// <param name="objectGuid">The unique identifier of the object that was not found.</param>
-        public OdmaObjectNotFoundException(string message, OdmaGuid objectGuid) 
+        /// <param name="repositoryId">The unique identifier of the repository.</param>
+        /// <param name="objectId">The unique identifier of the object that was not found or null if the repository does not exist.</param>
+        public OdmaObjectNotFoundException(string message, OdmaId repositoryId, OdmaId objectId) 
             : base(message)
         {
-            this._objectGuid = objectGuid;
+            this._repositoryId = repositoryId;
+            this._objectId = objectId;
         }
 
         /// <summary>
@@ -39,17 +55,24 @@ namespace OpenDMA.Api
         /// </summary>
         /// <param name="message">The message that describes the error.</param>
         /// <param name="innerException">The exception that is the cause of the current exception.</param>
-        /// <param name="objectGuid">The unique identifier of the object that was not found.</param>
+        /// <param name="repositoryId">The unique identifier of the repository.</param>
+        /// <param name="objectId">The unique identifier of the object that was not found or null if the repository does not exist.</param>
         public OdmaObjectNotFoundException(string message, Exception innerException, OdmaGuid objectGuid) 
             : base(message, innerException)
         {
-            this._objectGuid = objectGuid;
+            this._repositoryId = repositoryId;
+            this._objectId = objectId;
         }
 
         /// <summary>
-        /// Gets the unique identifier of the object that was not found.
+        /// Gets the unique identifier of the repository
         /// </summary>
-        public OdmaGuid? ObjectGuid => _objectGuid;
+        public OdmaId? RepositoryId => _repositoryId;
+
+        /// <summary>
+        /// Gets the unique identifier of the object that was not found or null if the repository has not been found
+        /// </summary>
+        public OdmaId? ObjectId => _objectId;
 
     }
 
